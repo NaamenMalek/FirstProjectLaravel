d4f89c4917a749d0bd89311b82387ed9fd2af2fe
NaamenMalek <malek.naamen@gmail.com>
2023-03-02 15:17:42 +0100

    second commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,6 +17,8 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
